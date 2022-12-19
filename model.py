292ca77ea2257c3040e57fe668c1b879297268da
Eric Ti Yu Chiang <serixscorpio@gmail.com>
2022-12-19 14:19:41 +0800

    Domain model add concept of deallocation.

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from attrs import define, frozen
+from attrs import define, field, frozen
 from datetime import date
 
 
@@ -9,12 +9,44 @@ class OrderLine:
     qty: int
 
 
-@define
+@define(eq=False)
 class Batch:
     reference: str
     sku: str
-    available_quantity: int
+    _purchased_quantity: int
     eta: date | None
+    _allocations: set[OrderLine] = field(init=False, factory=set)
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self) -> int:
+        return hash(self.reference)
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self._purchased_quantity >= line.qty
 
     def allocate(self, line: OrderLine):
-        self.available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self):
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self):
+        return self._purchased_quantity - self.allocated_quantity
+
+
+def allocate(line: OrderLine, batches: list[Batch]):
+    """Standalone allocate function."""
+    batch = next(b for b in sorted(batches) if batch.can_allocate(line))
+    batch.allocate(line)
+    return batch.reference
